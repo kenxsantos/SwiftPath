2f450954480a59dc1fcf6617e1e55bec7857e82e
Ken Santos <kenxsantos@gmail.com>
2024-10-02 23:52:57 +0800

    created .env file

--- lib/services/map_services.dart
+++ lib/services/map_services.dart
@@ -1,13 +1,12 @@
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-// import 'package:google_maps/constants.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:swiftpath/models/auto_complete_result.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 class MapServices {
-  final String key = "AIzaSyC2cU6RHwIR6JskX2GHe-Pwv1VepIHkLCg";
+  final String key = dotenv.env['GOOGLE_MAPS_API_KEY'] ?? '';
   final String types = 'geocode';
 
   Future<List<AutoCompleteResult>> searchPlaces(String searchInput) async {
@@ -16,11 +15,13 @@ class MapServices {
 
     var response = await http.get(Uri.parse(url));
 
-    var json = convert.jsonDecode(response.body);
-
-    var results = json['predictions'] as List;
-
-    return results.map((e) => AutoCompleteResult.fromJson(e)).toList();
+    if (response.statusCode == 200) {
+      var json = convert.jsonDecode(response.body);
+      var results = json['predictions'] as List;
+      return results.map((e) => AutoCompleteResult.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to load predictions');
+    }
   }
 
   Future<Map<String, dynamic>> getPlace(String? input) async {
@@ -29,11 +30,13 @@ class MapServices {
 
     var response = await http.get(Uri.parse(url));
 
-    var json = convert.jsonDecode(response.body);
-
-    var results = json['result'] as Map<String, dynamic>;
-
-    return results;
+    if (response.statusCode == 200) {
+      var json = convert.jsonDecode(response.body);
+      var results = json['result'] as Map<String, dynamic>;
+      return results;
+    } else {
+      throw Exception('Failed to load place details');
+    }
   }
 
   Future<Map<String, dynamic>> getDirections(
@@ -43,19 +46,22 @@ class MapServices {
 
     var response = await http.get(Uri.parse(url));
 
-    var json = convert.jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      var json = convert.jsonDecode(response.body);
+      var results = {
+        'bounds_ne': json['routes'][0]['bounds']['northeast'],
+        'bounds_sw': json['routes'][0]['bounds']['southwest'],
+        'start_location': json['routes'][0]['legs'][0]['start_location'],
+        'end_location': json['routes'][0]['legs'][0]['end_location'],
+        'polyline': json['routes'][0]['overview_polyline']['points'],
+        'polyline_decoded': PolylinePoints()
+            .decodePolyline(json['routes'][0]['overview_polyline']['points'])
+      };
 
-    var results = {
-      'bounds_ne': json['routes'][0]['bounds']['northeast'],
-      'bounds_sw': json['routes'][0]['bounds']['southwest'],
-      'start_location': json['routes'][0]['legs'][0]['start_location'],
-      'end_location': json['routes'][0]['legs'][0]['end_location'],
-      'polyline': json['routes'][0]['overview_polyline']['points'],
-      'polyline_decoded': PolylinePoints()
-          .decodePolyline(json['routes'][0]['overview_polyline']['points'])
-    };
-
-    return results;
+      return results;
+    } else {
+      throw Exception('Failed to load directions');
+    }
   }
 
   Future<dynamic> getPlaceDetails(LatLng coords, int radius) async {
@@ -67,9 +73,12 @@ class MapServices {
 
     var response = await http.get(Uri.parse(url));
 
-    var json = convert.jsonDecode(response.body);
-
-    return json;
+    if (response.statusCode == 200) {
+      var json = convert.jsonDecode(response.body);
+      return json;
+    } else {
+      throw Exception('Failed to load place details');
+    }
   }
 
   Future<dynamic> getMorePlaceDetails(String token) async {
@@ -78,8 +87,11 @@ class MapServices {
 
     var response = await http.get(Uri.parse(url));
 
-    var json = convert.jsonDecode(response.body);
-
-    return json;
+    if (response.statusCode == 200) {
+      var json = convert.jsonDecode(response.body);
+      return json;
+    } else {
+      throw Exception('Failed to load more place details');
+    }
   }
 }
