610d8ce39a86307573d3a8b33421e239d2f28f97
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2024-10-05 18:26:09 +0800

    add tab navigation bar

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -88,132 +88,173 @@ class _DashboardPageState extends State<DashboardPage>
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Welcome, ${_auth.currentUser?.displayName ?? 'User'}!',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  'You are logged in successfully.',
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.blue,
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/splash-screen');
-                  },
-                  child: const Text('Go To Maps'),
-                )
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
               ],
             ),
+            title: const Text('Tabs Demo'),
           ),
-        ),
-        SlideTransition(
-          position: _slideAnimation,
-          child: Material(
-            elevation: 16.0,
-            child: Container(
-              width: double.infinity,
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+          body: Stack(
+            children: [
+              TabBarView(
                 children: [
-                  const SizedBox(height: 50),
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: IconButton(
-                      icon: const Icon(Icons.close),
-                      onPressed: _toggleMenu,
+                  // For car icon (navigate to "/maps")
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/maps');
+                    },
+                    child: const Center(
+                      child: Icon(Icons.directions_car, size: 100),
                     ),
                   ),
-                  // Add the image here
-                  Center(
-                    child: Image.asset(
-                      'assets/images/Ambulance_icon.png',
-                      height:
-                          150, // You can adjust the height and width as necessary
-                      width: 300,
+
+                  // For transit icon (navigate to "/incident-report")
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/incident-report');
+                    },
+                    child: const Center(
+                      child: Icon(Icons.directions_transit, size: 100),
                     ),
                   ),
-                  Center(
-                    child: Text(
-                      'SWIFTPATH ${_auth.currentUser?.displayName ?? ''}!',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/edit-profile');
-                    },
-                    leading: const Icon(Icons.person),
-                    title: const Text('Profile'),
-                  ),
-                  ListTile(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/route-history');
-                    },
-                    leading: const Icon(Icons.history),
-                    title: const Text('Route History'),
-                  ),
-                  ListTile(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/report-history');
-                    },
-                    leading: const Icon(Icons.pending_actions_outlined),
-                    title: const Text('Report History'),
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.logout),
-                    title: const Text('Logout'),
-                    onTap: () => _signOut(context),
+
+                  // For bike icon (default icon, no navigation)
+                  const Center(
+                    child: Icon(Icons.directions_bike, size: 100),
                   ),
                 ],
               ),
-            ),
-          ),
-        ),
-        if (!_isMenuOpen)
-          Positioned(
-            top: 40.0,
-            left: 16.0,
-            child: GestureDetector(
-              onTap: _toggleMenu,
-              child: Container(
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(8.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      spreadRadius: 2,
-                      blurRadius: 8,
-                      offset: const Offset(0, 3),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Welcome, ${_auth.currentUser?.displayName ?? 'User'}!',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'You are logged in successfully.',
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.blue,
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/splash-screen');
+                      },
+                      child: const Text('Go To Maps'),
                     ),
                   ],
                 ),
-                child: const Icon(
-                  Icons.menu,
-                  color: Colors.black,
-                  size: 32,
+              ),
+              SlideTransition(
+                position: _slideAnimation,
+                child: Material(
+                  elevation: 16.0,
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 50),
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: IconButton(
+                            icon: const Icon(Icons.close),
+                            onPressed: _toggleMenu,
+                          ),
+                        ),
+                        Center(
+                          child: Image.asset(
+                            'assets/images/Ambulance_icon.png',
+                            height: 150,
+                            width: 300,
+                          ),
+                        ),
+                        Center(
+                          child: Text(
+                            'SWIFTPATH ${_auth.currentUser?.displayName ?? ''}!',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/edit-profile');
+                          },
+                          leading: const Icon(Icons.person),
+                          title: const Text('Profile'),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/route-history');
+                          },
+                          leading: const Icon(Icons.history),
+                          title: const Text('Route History'),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/report-history');
+                          },
+                          leading: const Icon(Icons.pending_actions_outlined),
+                          title: const Text('Report History'),
+                        ),
+                        ListTile(
+                          leading: const Icon(Icons.logout),
+                          title: const Text('Logout'),
+                          onTap: () => _signOut(context),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
+              if (!_isMenuOpen)
+                Positioned(
+                  top: 40.0,
+                  left: 16.0,
+                  child: GestureDetector(
+                    onTap: _toggleMenu,
+                    child: Container(
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(8.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 2,
+                            blurRadius: 8,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.menu,
+                        color: Colors.black,
+                        size: 32,
+                      ),
+                    ),
+                  ),
+                ),
+            ],
           ),
-      ],
+        ),
+      ),
     );
   }
 }
