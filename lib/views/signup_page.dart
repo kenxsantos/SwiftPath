019398e116316b5becd51a7620c5d92786a7b920
Ken Santos <kenxsantos@gmail.com>
2024-12-07 19:41:22 +0800

    refactor authentication flow: add email verification and update navigation after signup

--- lib/views/signup_page.dart
+++ lib/views/signup_page.dart
@@ -144,8 +144,7 @@ class _SignUpPageState extends State<SignUpPage> {
                                   Navigator.push(
                                     context,
                                     MaterialPageRoute(
-                                      builder: (context) =>
-                                          const SplashScreen(),
+                                      builder: (context) => const LoginPage(),
                                     ),
                                   );
                                 });
